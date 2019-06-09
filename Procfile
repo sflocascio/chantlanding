060dd191a12e5dae549b01772a7c62da29583dee
Sam LoCascio <sflocascio@gmail.com>
2019-06-09 10:32:01 -0400

    preparing for heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+
+gunicorn chantapp.wsgi
--- chantapp/settings.py
+++ chantapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#What sam added 
+STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+#end what sam added
+
+#WhiteNoise Static Storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+django_heroku.settings(locals())
