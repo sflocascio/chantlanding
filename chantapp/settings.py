a084320103d2268ea8b8daf2190ba7942807094a
Sam LoCascio <sflocascio@gmail.com>
2019-06-09 10:42:12 -0400

    heroku hot fix

--- chantapp/settings.py
+++ chantapp/settings.py
@@ -122,11 +122,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+#adding from docs
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+# end add from docs 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 #What sam added 
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
